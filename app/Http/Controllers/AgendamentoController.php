dd66ae23a8fe95de0d11fbb53e132f16caa8c81e
unknown <Administrador@PC1-009-015690.academico.local>
2022-10-22 15:33:18 -0300

    ajustes pontuais

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -13,32 +13,57 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class AgendamentoController extends Controller
 {
+
+    private function publicQueue($id){
+        $connection = new AMQPStreamConnection('192.168.18.9', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('notificationScheduleQueue', false, false, false, false);
+        
+        
+        $msg = new AMQPMessage(intval($id));
+        
+        $channel->basic_publish($msg, '', 'notificationScheduleQueue');
+        
+        $channel->close();
+        $connection->close();
+    }
+
     public function Create(Request $request){
 
         $startTime = Carbon::parse($request->startedAt);
         $endTime = Carbon::parse($request->finishedAt);
 
-        $totalHours = $endTime->diffInHours($startTime);
-        if($totalHours == 0){
-            $totalHours = 1;
-        }
-
-        if($endTime <= $startTime)
+        if($endTime <= $startTime){
             return response(json_encode(
                 array(
                     'message' => 'A data final tem que ser maior que a data inicial',
                     'status' => 403
                 )
             ), 403);
+        }  
+        
+        //Define a quantidade de horas;
+        $totalHours = $endTime->diffInHours($startTime);
+        if($totalHours == 0){
+            $totalHours = 1;
+        }
+        //Define o valor do aluguel;
+        $amount = $totalHours * 80;
+        
+         
+        //Verifica se já existe um aluguel nesse horario; 
+        $startTimeVerify =  Carbon::parse($request->startedAt)->addMinutes(3);
+        $endTimeVerify =  Carbon::parse($request->finishedAt)->subMinutes(3);
 
         $data = DB::table('scheduler')->select()
-        ->where(function ($query) use ($startTime) {
+        ->where(function ($query) use ($startTime, $startTimeVerify) {
             $query->where('startedAt', '<=', $startTime)
-                  ->Where('finishedAt', '>=', $startTime);
+                  ->Where('finishedAt', '>=', $startTimeVerify);
         })
-        ->orwhere(function ($query) use ($endTime) {
+        ->orwhere(function ($query) use ($endTime, $endTimeVerify) {
             $query->where('startedAt', '<=', $endTime)
-                  ->where('finishedAt', '>=', $endTime);
+                  ->where('finishedAt', '>=', $endTimeVerify);
         })
         ->count('id');
         
@@ -58,23 +83,14 @@ class AgendamentoController extends Controller
             $agendamento->userId = $request->userId;
             $agendamento->startedAt = $startTime;
             $agendamento->finishedAt = $endTime;
-            $agendamento->amount = 80 * $totalHours;
+            $agendamento->amount = $amount;
             $agendamento->save();
             
             DB::commit();
 
-            $connection = new AMQPStreamConnection('192.168.18.9', 5672, 'guest', 'guest');
-            $channel = $connection->channel();
-
-            $channel->queue_declare('notificationScheduleQueue', false, false, false, false);
-            
-
-            $msg = new AMQPMessage($agendamento->id);
-            
-            $channel->basic_publish($msg, '', 'notificationScheduleQueue');
-            
-            $channel->close();
-            $connection->close();
+            if(isset($agendamento->id) && !empty($agendamento->id)){
+                $this->publicQueue($agendamento->id);
+            }
 
             return response($agendamento, 200);
         } catch (\Exception $e) {
